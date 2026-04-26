6ed011ac504e77f15de6d9df8a0d5238390623ad
Prithiviraj25 <prithivirajn25@gmail.com>
2026-04-26 09:50:32 +0530

    app soruce change

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/royal-tamil-grace/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
